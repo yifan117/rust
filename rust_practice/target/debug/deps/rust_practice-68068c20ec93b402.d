73b7a77d47416e0a847d4c1475f2273e2c565229
yifan117 <yifan.qiu17@gmail.com>
2022-10-22 23:47:00 +1030

    cp

---
+++ rust_practice/target/debug/deps/rust_practice-68068c20ec93b402.d
@@ -0,0 +1,5 @@
+c:\Users\Yifan Qiu\Desktop\coding\rust\rust_practice\target\debug\deps\rust_practice-68068c20ec93b402.rmeta: src\main.rs
+
+c:\Users\Yifan Qiu\Desktop\coding\rust\rust_practice\target\debug\deps\rust_practice-68068c20ec93b402.d: src\main.rs
+
+src\main.rs:
---
+++ rust_practice/target/debug/deps/rust_practice-8989006e4a071350.d
@@ -0,0 +1,5 @@
+c:\Users\Yifan Qiu\Desktop\coding\rust\rust_practice\target\debug\deps\rust_practice-8989006e4a071350.rmeta: src\main.rs
+
+c:\Users\Yifan Qiu\Desktop\coding\rust\rust_practice\target\debug\deps\rust_practice-8989006e4a071350.d: src\main.rs
+
+src\main.rs:
