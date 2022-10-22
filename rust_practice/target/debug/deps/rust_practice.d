c9e733d201b5df74cf07aa65b8c3c160f880376c
yifan117 <yifan.qiu17@gmail.com>
2022-10-22 20:42:05 +1030

    init

---
+++ rust_practice/target/debug/deps/rust_practice.d
@@ -0,0 +1,5 @@
+C:\Users\Yifan Qiu\Desktop\coding\rust\rust_practice\target\debug\deps\rust_practice.exe: src\main.rs
+
+C:\Users\Yifan Qiu\Desktop\coding\rust\rust_practice\target\debug\deps\rust_practice.d: src\main.rs
+
+src\main.rs:
---
+++ rust_practice/target/debug/rust_practice.d
@@ -0,0 +1,1 @@
+C:\Users\Yifan\ Qiu\Desktop\coding\rust\rust_practice\target\debug\rust_practice.exe: C:\Users\Yifan\ Qiu\Desktop\coding\rust\rust_practice\src\main.rs
